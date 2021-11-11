595cdc25a1c982aeea6ac233742438abc213b18c
seokjooha <ferrari5712@gmail.com>
2021-11-11 15:56:20 +0900

    update

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/sha512"
 	"flag"
 	"fmt"
@@ -19,7 +20,7 @@ func hash(text string) string {
 }
 
 // pci 호출하는 부분
-func work(bar *mpb.Bar, total int, number int, wg *sync.WaitGroup, c chan int, str string) {
+func work(bar *mpb.Bar, total int, number int) {
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 	max := 100 * time.Millisecond
 	// FPGA card
@@ -27,6 +28,9 @@ func work(bar *mpb.Bar, total int, number int, wg *sync.WaitGroup, c chan int, s
 	time.Sleep(100 * time.Millisecond)
 	fmt.Printf("FPGA #%d ready!!!\n", number)
 	for i := 0; i < total; i++ {
+		if bar.Completed() {
+			break
+		}
 		// start variable is solely for EWMA calculation
 		start := time.Now()
 		bar.Increment()
@@ -36,34 +40,14 @@ func work(bar *mpb.Bar, total int, number int, wg *sync.WaitGroup, c chan int, s
 	}
 }
 
-func consoleRun() {
-	// set variables
-	maxFPGA := 4
-
-	text := flag.String("text", "", "암호 문자열")
-	numFPGA := flag.Int("numFPGA", 3, "FPGA 분산처리 숫자")
-	maxEnumerate := flag.Int("maxTry", 1000, "Number of FPGA Max Decrypt try")
-
-	c := make(chan int, *numFPGA)
-	flag.Parse()
-
-	if flag.NFlag() == 0 {
-		flag.Usage()
-		return
-	}
-
-	if *numFPGA > maxFPGA {
-		println("Max FPGA Number is", maxFPGA, "please retry.")
-		return
-	}
-	fmt.Println("Input text : ", *text)
-	fmt.Println("Number of FPGA : ", *numFPGA)
-	fmt.Printf("Input max try value: %d\n", *maxEnumerate)
+func consoleRun(maxEnumerate int, numFPGA int, text string) {
 	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+	defer cancel()
 
-	p := mpb.New(mpb.WithWaitGroup(&wg))
+	p := mpb.NewWithContext(ctx, mpb.WithWaitGroup(&wg))
 
-	total, numBars := *maxEnumerate, *numFPGA
+	total, numBars := maxEnumerate, numFPGA
 	wg.Add(numBars)
 
 	for i := 0; i < numBars; i++ {
@@ -88,16 +72,54 @@ func consoleRun() {
 		i := i
 		go func() {
 			defer wg.Done()
-			work(bar, total, i, &wg, c, *text)
+			work(bar, total, i)
 		}()
 	}
 	// Waiting for passed &wg and for all bars to complete and flush
 	p.Wait()
 
-	//result := hash(*text)
-	if len(c) > 0 {
-		fmt.Println(<-c)
+	result := hash(text)
+
+	fmt.Println("------------------------------------[ Decrypt Success ]------------------------------------")
+	fmt.Println("------------------------------------[ result ]---------------------------------------------")
+	fmt.Println(result)
+}
+
+func consoleFail(maxEnumerate int, numFPGA int) {
+	var wg sync.WaitGroup
+
+	p := mpb.New(mpb.WithWaitGroup(&wg))
+
+	total, numBars := maxEnumerate, numFPGA
+	wg.Add(numBars)
+
+	for i := 0; i < numBars; i++ {
+		name := fmt.Sprintf("FPGA#%d:", i)
+		bar := p.AddBar(int64(total),
+			mpb.PrependDecorators(
+				// simple name decorator
+				decor.Name(name),
+				decor.CountersNoUnit("%d / %d", decor.WCSyncWidth),
+				// decor.DSyncWidth bit enables column width synchronization
+				//decor.Percentage(decor.WCSyncSpace),
+			),
+			mpb.AppendDecorators(
+				// replace ETA decorator with "done" message, OnComplete event
+				decor.OnComplete(
+					// ETA decorator with ewma age of 60
+					decor.EwmaETA(decor.ET_STYLE_GO, 60, decor.WCSyncWidth), "Failed",
+				),
+			),
+		)
+		// simulating some work
+		i := i
+		go func() {
+			defer wg.Done()
+			work(bar, total, i)
+		}()
 	}
+	// Waiting for passed &wg and for all bars to complete and flush
+	p.Wait()
 
 	fmt.Println("------------------------------------[ Decrypt Failed ]------------------------------------")
 	//fmt.Println("------------------------------[ result ]------------------------------")
@@ -105,5 +127,34 @@ func consoleRun() {
 }
 
 func main() {
-	consoleRun()
+	// set variables
+	maxFPGA := 4
+
+	// Scan input arguments
+	text := flag.String("text", "", "암호 문자열")
+	numFPGA := flag.Int("numFPGA", 3, "FPGA 분산처리 숫자")
+	maxEnumerate := flag.Int("maxTry", 1000, "Number of FPGA Max Decrypt try")
+	success := flag.Bool("success", false, "decrypt success is true, fail is false")
+
+	flag.Parse()
+
+	// If input arguments empty print help
+	if flag.NFlag() == 0 {
+		flag.Usage()
+		return
+	}
+
+	if *numFPGA > maxFPGA {
+		println("Max FPGA Number is", maxFPGA, "please retry.")
+		return
+	}
+	fmt.Println("Input text : ", *text)
+	fmt.Println("Number of FPGA : ", *numFPGA)
+	fmt.Printf("Input max try value: %d\n", *maxEnumerate)
+
+	if *success == true {
+		consoleRun(*maxEnumerate, *numFPGA, *text)
+	} else {
+		consoleFail(*maxEnumerate, *numFPGA)
+	}
 }
